f7f6d097b7bc18bd1af5437aa34e40ce7ad367cb
3m4nr3su <hillaryotieno97@gmail.com>
2022-06-07 14:47:39 +0000

    k

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    count = 0
+    for num in my_list:
+        count += 1
+        if count == 0:
+            return None
+        my_list.sort()
+        return my_list[-1]
